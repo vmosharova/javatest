1b6c6856434a09ee509996f16f7e964848e5b55e
Varvara Mosharova <varvara.mosharova@schaltzeit.com>
2020-12-05 22:53:23 +0300

    Добавлено несколько функций

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("world");
+        hello("user");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной" + l + "=" + area(l));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello" + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len + len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 
 }
