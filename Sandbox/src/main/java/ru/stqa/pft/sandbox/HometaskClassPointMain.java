e3635184226407c3b60a3de40ce677ce1b65482d
Varvara Mosharova <varvara.mosharova@schaltzeit.com>
2020-12-06 19:42:12 +0300

    Добавлен класс Point и функция distance внутри MyFirstProgram (пока не метод класса Point)

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/HometaskClassPointMain.java
@@ -0,0 +1,15 @@
+/***package ru.stqa.pft.sandbox;
+
+public class HometaskClassPointMain {
+
+    public static void main(String[] args) {
+
+        public static double distance(Point p1, Point p2){
+            Point p1 = new Point(1, 2);
+            Point p2 = new Point(3, 4);
+            double result = (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+            return ...
+            System.out.println(Math.sqrt(result));
+        }
+    }
+}***/
