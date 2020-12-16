6aacc1f6188b99d6a4c6c9b80f922854478e7f1b
Varvara Mosharova <varvara.mosharova@schaltzeit.com>
2020-12-16 20:02:40 +0300

    Добавлен новый класс, атрибуты которого передаются в первый тест (на создание Groups)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -28,7 +28,7 @@ public class GroupCreationTests {
   public void testGroupCreation() throws Exception {
     gotoGroupPage();
     initGroupCreation();
-    fillGroupForm("test1", "test2", "test3");
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
     returntoGroupPage();
   }
@@ -41,17 +41,17 @@ public class GroupCreationTests {
     wd.findElement(By.linkText("group page")).click();
   }
 
-  private void fillGroupForm(String name, String header, String footer) {
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(name);
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(header);
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(footer);
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     wd.findElement(By.name("submit")).click();
   }
 
