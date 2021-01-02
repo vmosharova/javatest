a605b43b379c094433c7b8be5f7728ce113e9f26
Varvara Mosharova <varvara.mosharova@schaltzeit.com>
2021-01-03 00:09:34 +0300

    Рефакторинг ContactHelper, рефакторинг ContactCreationTests (небольшая оптимизация кода)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,22 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.NewContact;
 
-public class ContactHelper {
+public class ContactHelper extends HelperBase {
 
-    private WebDriver wd;
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void newContactCreationWithNameMidnameSurname(NewContact newContact) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(newContact.getName());
-      wd.findElement(By.name("middlename")).clear();
-      wd.findElement(By.name("middlename")).sendKeys(newContact.getMiddlename());
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(newContact.getSurname());
-      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+        type(By.name("firstname"), newContact.getName());
+        type(By.name("middlename"), newContact.getMiddlename());
+        type(By.name("lastname"), newContact.getSurname());
+        click(By.name("submit"));
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,9 +7,8 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() throws Exception {
-
         app.getNavigationHelper().gotoCreateNewContactPage();
-        app.getContactHelper().newContactCreationWithNameMidnameSurname(new NewContact("Name", "MidName", "Surname"));
+        app.getContactHelper().newContactCreationWithNameMidnameSurname(new NewContact("Name1", "MidName2", "Surname3"));
         app.returntoHomePage();
         app.getSessionHelper().logOut();
     }
