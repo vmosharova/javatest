1e1a66535a1b1efb6de9af428fe6ac6e3a78d2cb
Varvara Mosharova <varya.mosharova@gmail.com>
2021-03-06 18:05:04 +0300

    Group selection by index added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()) {      //если группы нет, создаём её
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
         app.getGroupHelper().submitGroupModification();
