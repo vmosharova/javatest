00d777277855fad9f95ed9d5273daa79bc68e2d3
Varvara Mosharova <varya.mosharova@gmail.com>
2021-03-07 20:53:15 +0300

    equals() and toString() for GroupData added; + assertion in GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -20,6 +20,13 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().returntoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
+    /*Или вот такой assertion:
+    for (int i = 0; i < after.size(); i++) {
+      Assert.assertEquals(before.get(i), after.get(i));
+    }*/
   }
 
 
