6e82db48e10845704b1e834b3788b7fac224c0a0
Varvara Mosharova <varya.mosharova@gmail.com>
2021-03-10 14:14:57 +0300

    GroupData ID comparator as a lambda function added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +14,29 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1); // Сравниваем размеры списка до и после
 
-    int max = 0;
+   /* int max = 0; // старый способ вычисления максимального идентификатора
     for (GroupData g : after) {
       if (g.getId() > max) {
         max = g.getId();
       }
     }
-    group.setId(max);
+
+
+    Comparator<? super GroupData> byId = new Comparator<GroupData>() { // новый способ сравнения при помощи компаратора; то же, что выше в лямбда-выражении
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId())
+      }
+    } */
+
+    int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+    group.setId(max1);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
