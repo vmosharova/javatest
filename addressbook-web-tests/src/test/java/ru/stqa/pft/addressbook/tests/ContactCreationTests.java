bf0e231d4d8cc77281299b837122480ea38c8443
Varvara Mosharova <varya.mosharova@gmail.com>
2021-04-24 22:23:09 +0300

    Added reading test data from a .csv file for Contact Creation Tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,9 +3,10 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.NewContact;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,27 +17,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         File photo = new File("src/test/resources/stru.png");
-        list.add(new Object[]{new NewContact().withName("name1")
-                .withMiddlename("midname1").withSurname("surname 1")
-                .withAddress("address 1").withFirstEmail("mail1")
-                .withSecondEmail("mail12").withThirdEmail("mail13")
-                .withHomePhone("phone1").withMobilePhone("phone12").withWorkPhone("phone13")
-                .withGroup("test1").withPhoto(photo)});
-        list.add(new Object[]{new NewContact().withName("name2")
-                .withMiddlename("midname2").withSurname("surname 2")
-                .withAddress("address 2").withFirstEmail("mail2")
-                .withSecondEmail("mail22").withThirdEmail("mail23")
-                .withHomePhone("phone2").withMobilePhone("phone22").withWorkPhone("phone33")
-                .withGroup("test2").withPhoto(photo)});
-        list.add(new Object[]{new NewContact().withName("name3")
-                .withMiddlename("midname3").withSurname("surname 3")
-                .withAddress("address 3").withFirstEmail("mail3")
-                .withSecondEmail("mail32").withThirdEmail("mail33")
-                .withHomePhone("phone3").withMobilePhone("phone32").withWorkPhone("phone33")
-                .withGroup("test3").withPhoto(photo)});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new NewContact().withName(split[0]).withMiddlename(split[1]).withSurname(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
