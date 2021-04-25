a00bb10c8242231af5ace6b6b9a8775f79789b3f
Varvara Mosharova <varya.mosharova@gmail.com>
2021-04-25 22:35:10 +0300

    6.10 Added properties in a config file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
     // protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
     // protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
