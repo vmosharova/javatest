c4cba8fc076a298f2d44be5839de5c304c66017f
Varvara Mosharova <varya.mosharova@gmail.com>
2021-05-01 23:11:08 +0300

    Fetching data from the db with DbHelper() for ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -57,11 +57,11 @@ public class ContactCreationTests extends TestBase {
     @Test(dataProvider = "validContactsFromJson")
     public void testContactCreation(NewContact contact) throws Exception {
         app.goTo().homePage();
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         app.goTo().createNewContactPage();
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
         app.goTo().homePage();
 
         assertThat(after, equalTo(
