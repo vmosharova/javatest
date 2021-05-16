244bc117a27c1d64f7fe757fb003dbb3559c81c7
Varvara Mosharova <varya.mosharova@gmail.com>
2021-05-16 16:09:26 +0300

    NEW TEST INFRASTRUCTURE is added for Mantis + new module for Mantis tests

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+    // protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+    // protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+
+    }
+}
