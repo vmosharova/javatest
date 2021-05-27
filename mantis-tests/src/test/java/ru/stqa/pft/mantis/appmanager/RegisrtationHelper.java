15501349caaaf6c802fbbf40e28dc7a9d13f21ee
Varvara Mosharova <varya.mosharova@gmail.com>
2021-05-27 21:56:07 +0300

    Modified RegistrationHelper() so it inherits HelperBase()

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegisrtationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegisrtationHelper.java
@@ -1,17 +1,24 @@
-package ru.stqa.pft.mantis.appmanager;
+ package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+ import org.openqa.selenium.By;
 
-public class RegisrtationHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+ public class RegisrtationHelper extends HelperBase {
 
     public RegisrtationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
+        super(app);
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl") + "signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[type='submit']"));
+    }
+
+    public void finish(String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("input[value='Update User']"));
     }
 }
